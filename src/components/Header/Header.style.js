dfab201452106499d57d88fdee148522ea4b99fa
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-06 23:20:51 -0300

    style: add styles

--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,11 +1,14 @@
 import styled from "styled-components";
-import bgHeaderMobile from "../../assets/images/bg-header-mobile.svg";
 
 export const Head = styled.header`
   width: 100%;
   height: 15rem;
   background-color: ${({ theme }) => theme.colors.primary};
-  background-image: url(${bgHeaderMobile});
+  background-image: url("/images/bg-header-mobile.svg");
   background-repeat: no-repeat;
   background-size: cover;
+
+  @media screen and (min-width: 560px) {
+    background-image: url("/images/bg-header-desktop.svg");
+  }
 `;
