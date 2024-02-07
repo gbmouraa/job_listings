001f385523e77fb0dbdf9f4e1ab44e706ed4933f
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-07 12:40:48 -0300

    feat: add StaticJobsProvider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import StaticJobsProvider from "./context/StaticJobs";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalStyle from "./styles/global";
@@ -8,9 +9,11 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <StaticJobsProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </StaticJobsProvider>
   </React.StrictMode>
 );
