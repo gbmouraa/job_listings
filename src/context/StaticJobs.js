2b7fc6c9c850707b16ca1a3cbce192e26447d247
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2024-02-09 12:19:02 -0300

    chore: add comment

--- src/context/StaticJobs.js
+++ src/context/StaticJobs.js
@@ -4,6 +4,7 @@ import data from "../assets/data.json";
 export const StaticJobsContext = createContext();
 
 function StaticJobsProvider({ children }) {
+  // cria uma propriedade do que é filtravel no job(emprego)
   const allJobs = data.map((job) => {
     job.filterableCategories = [
       job.role,
@@ -18,8 +19,11 @@ function StaticJobsProvider({ children }) {
   const [categories, setCategories] = useState([]);
 
   const filteredJobs = allJobs.filter((job) => {
+    // caso categories seja vazio, sempre sera retornado true e a callback não é executada
     return categories.every((item) => job.filterableCategories.includes(item));
   });
+
+  console.log(filteredJobs);
 
   useEffect(() => {
     categories.length > 0 ? setShowFilters(true) : setShowFilters(false);
